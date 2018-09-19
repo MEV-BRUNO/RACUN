5e3fa6d5e37186cf44b888963fa9c54b521a02f0
Josipa Kovač <josipa.kovac@mev.hr>
2018-09-19 20:52:31 +0200

    Dodavanje view-a za racune

---
+++ Racun/Racun/Controllers/ReceiptController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using Microsoft.Ajax.Utilities;
+using Racun.DbCtx;
+using Racun.Models;
+
+namespace Racun.Controllers
+        public ActionResult Index()
+{
+    return View("Index", Receipt);
+}
+        
